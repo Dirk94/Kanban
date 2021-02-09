2c697f3d2ed8f8aaceeda0f0ff7a4a8dff36888b
Dirk Hoekstra <dirkhoekstra@gmail.com>
2021-02-09 13:20:45 +0100

    add register api endpoint

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Simply route everything to the Vue view.
+// Simply route everything except API calls to the Vue view.
 Route::get('{any}', function () {
     return view('vue-application');
-})->where('any', '(.*)');
+})->where('any', '^(?!\/api).*$');
